df238be81f115265abe120e233227e58c57a8d4f
raksul-sian-izm <s.izumida@raksul.com>
2019-05-27 15:05:45 +0700

    define root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/help'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
