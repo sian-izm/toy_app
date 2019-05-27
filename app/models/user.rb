2445fcad08a53e389949bf9f410605eb72c9f9e4
raksul-sian-izm <s.izumida@raksul.com>
2019-05-27 15:54:44 +0700

    introduce devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :microposts
-  validates :name,  presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'static_pages#home'
   get 'static_pages/help'
   resources :microposts
